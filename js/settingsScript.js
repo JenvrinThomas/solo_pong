5eae6eb9edb7ea0062e82620cebd79798a114a30
Thomas Jenvrin <thomas.jenvrin@etu.unicaen.fr>
2025-09-24 17:59:04 +0200

    Correction d'un erreur de syntaxe dans settingsScript.js, empêchant le fonctionnement de la page

--- js/settingsScript.js
+++ js/settingsScript.js
@@ -1,7 +1,7 @@
 document.getElementById('confirmChanges').addEventListener('click', () => {
     // à faire : enregistrer les paramètres
     document.location.href = "index.html";
-}
+});
 
 function refreshValues() {
     const speedRange = document.getElementById('speedRange');
